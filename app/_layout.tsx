ed2d686d6fc4563b63b96964da3e30933e4269cb
valentincharlone <valencharlone@gmail.com>
2025-08-01 17:05:11 -0300

    feat: implement tab navigation and layout structure with icons and images

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,14 @@
 import { Stack } from "expo-router";
-import './global.css';
+import "./global.css";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="movies/[id]" options={{ headerShown: false }} />
+      <Stack.Screen name="saved" options={{ headerShown: false }} />
+      <Stack.Screen name="search" options={{ headerShown: false }} />
+      <Stack.Screen name="profile" options={{ headerShown: false }} />
+    </Stack>
+  );
 }
