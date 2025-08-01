5c71c3e9a25c2f690b3baf4b1c2bfe88a7acce61
valentincharlone <valencharlone@gmail.com>
2025-07-31 22:53:19 -0300

    feat: integrate NativeWind for styling and add Tailwind CSS configuration
    - Added global CSS file for Tailwind directives.
    - Refactored main component to use NativeWind classes for styling.
    - Created Babel and Metro configurations for NativeWind support.
    - Added TypeScript definitions for NativeWind.
    - Updated package dependencies to include NativeWind and Tailwind CSS.
    - Created Tailwind CSS configuration file with custom theme colors.
    - Updated TypeScript configuration to include NativeWind types.

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,9 @@
 import { Text, View } from "react-native";
 
-export default function Index() {
+export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Welcome</Text>
+    <View className="flex-1 items-center justify-center ">
+      <Text className="text-5xl font-bold text-light-200">Welcome!</Text>
     </View>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // NOTE: Update this to include the paths to all files that contain Nativewind classes.
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "*.{js,ts,jsx,tsx,mdx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#030014",
+        secondary: "#151312",
+        light: {
+          100: "#d6c6ff",
+          200: "#a8b5db",
+          300: "#9ca4ab",
+        },
+        dark: {
+          100: "#221f3d",
+          200: "#0f0d23",
+        },
+        accent: "#AB8BFF",
+      },
+    },
+  },
+  plugins: [],
+};
